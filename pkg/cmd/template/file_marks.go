850a173fafcb27d060be07a9bf7eb685b13d7151
TLDMain <119927819+TLDMain@users.noreply.github.com>
2026-04-16 11:36:29 +0300

    feat: add flag to allow skipping unmatched wildcard file marks

--- pkg/cmd/template/file_marks.go
+++ pkg/cmd/template/file_marks.go
@@ -12,13 +12,15 @@ import (
 )
 
 type FileMarksOpts struct {
-	FileMarks []string
+	FileMarks  []string
+	StarStrict bool
 }
 
 // Set registers file mark flags and wires-up those flags up to this
 // FileMarksOpts to be set when the corresponding cobra.Command is executed.
 func (s *FileMarksOpts) Set(cmdFlags CmdFlags) {
 	cmdFlags.StringArrayVar(&s.FileMarks, "file-mark", nil, "File mark (ie change file path, mark as non-template) (format: file:key=value) (can be specified multiple times)")
+	cmdFlags.BoolVar(&s.StarStrict, "file-mark-strict", true, "Error if a file path pattern containing '*' matches no files")
 }
 
 func (s *FileMarksOpts) Apply(filesToProcess []*files.File) ([]*files.File, error) {
@@ -101,7 +103,7 @@ func (s *FileMarksOpts) Apply(filesToProcess []*files.File) ([]*files.File, erro
 			}
 		}
 
-		if !matched {
+		if !matched && (s.StarStrict || !strings.Contains(path, "*")) {
 			return nil, fmt.Errorf("Expected file mark '%s' to match at least one file by path, but did not", mark)
 		}
 
